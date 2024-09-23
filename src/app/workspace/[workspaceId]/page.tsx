d26d3387431fc345d286b899533692fecd6fa712
Dinh Huy Hoang <80768175+Hoangxt@users.noreply.github.com>
2024-09-23 17:29:25 +0700

    Toolbar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,19 @@
 'use client';
 
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+import { useEffect, useMemo } from 'react';
+import { Loader, TriangleAlert } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+// call id from db using hook
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+// call api
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 
-const WorkspaceIdPage = ({ params }: WorkspaceIdPageProps) => {
-  return <div>Workspace ID: {params.workspaceId}</div>;
+const WorkspaceIdPage = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
+  return <div>Data: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
